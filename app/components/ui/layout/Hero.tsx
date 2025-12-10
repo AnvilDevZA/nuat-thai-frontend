803c01ab750cc5270d322eb8bddc8b385afa14cb
AnvilDevZA <websmith@anvil-dev.site>
2025-12-10 20:02:57 +0200

    updated logo

--- app/components/ui/layout/Hero.tsx
+++ app/components/ui/layout/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
   <div className="hero-overlay opacity-60"></div>
   <div className="hero-content text-neutral-content text-center">
     <div className="max-w-md">
-      <img src="/nuat-thai.svg" height="256" width="256" className="m-auto text-center" />
+      <img src="/logo.svg" height="256" width="256" className="m-auto text-center" />
       <h1 className="mb-4 text-4xl text-[#ffb300] uppercase leading-tight font-serif font-bold">Nuat Thai</h1>
       <p className="mb-4 uppercase font-sans font-light text-md">
        Authentic Thai Massage by a Qualified Therapist from Thailand
