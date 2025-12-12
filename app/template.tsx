9fcea32a8df679bc48b937d762a35ec5d7f2c2ea
AnvilDevZA <websmith@anvil-dev.site>
2025-12-12 13:18:21 +0200

    Animation Test

--- app/template.tsx
+++ app/template.tsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 
 const variants = {
     hidden: { opacity: 0, x: -200, y: 0 },
-    enter: { opacity: 1, x: 0, y: 0},
+    enter: { opacity: 0.7, x: 0, y: 0},
 };
 
 export default function Template({ children}: {children: React.ReactNode }) {
