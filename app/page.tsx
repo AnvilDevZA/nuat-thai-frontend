d9cdbe5ef924900c30d05c96860e27fd3d45317c
AnvilDevZA <websmith@anvil-dev.site>
2025-12-12 12:09:06 +0200

    Adding more content

--- app/page.tsx
+++ app/page.tsx
@@ -9,10 +9,13 @@ export default function Home() {
       <Nav />
       <Hero />
       <main className="bg-[#fdf1e4] text-[#8f143d]">
-        <article className="max-w-2xl m-auto py-10">
+        <article className="max-w-2xl m-auto py-10 px-6">
           <h1 className="font-black font-serif uppercase text-center text-2xl mb-6">Blending tradition with modern science</h1>
-          <p className="text-lg font-sans font-light uppercase text-center leading-relaxed">
+          <p className="text-lg font-sans font-light uppercase text-center leading-relaxed mb-4">
             Nuat Thai offers traditional Thai Massage Therapy known as a complimentary medicine for pain, muscle trauma, and stress. Our therapist - <em>Sri</em> Jintana Suipae is trained with the Royal Thai Medical board and has assisted numerous people with dealing with chronic and acute pain both in Thailand and South Africa</p>
+          <h2 className="font black font-serif uppercase text-center text-2xl mb-6">
+            Our Holiday Specials
+          </h2>
         </article>
       </main>
       <Footer />
