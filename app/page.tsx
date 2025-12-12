7e6319848eb4c9304b66bbeeac140a27e41efa2e
AnvilDevZA <websmith@anvil-dev.site>
2025-12-12 12:06:41 +0200

    Fix styling

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <main className="bg-[#fdf1e4] text-[#8f143d]">
         <article className="max-w-2xl m-auto py-10">
           <h1 className="font-black font-serif uppercase text-center text-2xl mb-6">Blending tradition with modern science</h1>
-          <p className="text-xl font-serif font-light uppercase text-center leading-relaxed">
+          <p className="text-lg font-sans font-light uppercase text-center leading-relaxed">
             Nuat Thai offers traditional Thai Massage Therapy known as a complimentary medicine for pain, muscle trauma, and stress. Our therapist - <em>Sri</em> Jintana Suipae is trained with the Royal Thai Medical board and has assisted numerous people with dealing with chronic and acute pain both in Thailand and South Africa</p>
         </article>
       </main>
